b581088a1a2e8d484fce8a99a68e83a740e3f334
Thomas DiNaso <31248559+TDinaso559@users.noreply.github.com>
2017-09-06 16:24:57 -0500

    Add files via upload

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,17 +7,33 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config'),
+    listings = require('./listings.json');
 
 /* Connect to your database */
+mongoose.connect(config.db.uri);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
- */
 
-
-/* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
  */
+
+// reading from listings.json and put in database
+var listEntries = listings["entries"]
+
+for (var key in listEntries) {
+  var listingJSON = listEntries[key];
+  var dbListing = new Listing({
+    name: listingJSON.name,
+    code: listingJSON.code,
+    address: listingJSON.address,
+    coordinates: listingJSON.coordinates
+  });
+
+  dbListing.save(function(errSave) {
+    if (errSave) { throw errSave; }
+  });
+} 
--- queries.js
+++ queries.js
@@ -1,28 +1,64 @@
 /* Fill out these functions using Mongoose queries*/
 
-var findLibraryWest = function() {
-  /* 
+// setup variables
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  Listing = require('./ListingSchema.js'),
+  config = require('./config'),
+  listings = require('./listings.json');
+
+/* Connect to your database */
+mongoose.connect(config.db.uri);
+ /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
-};
-var removeCable = function() {
-  /*
+var findLibraryWest = function() {
+ 
+  Listing.findOne({ name: 'Library West' }, function(err, listing) {
+    if (err == null && listing != null) {
+      console.log(listing);
+    }
+  });
+}; 
+ /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+
+var removeCable = function() {
+  Listing.findOne({ code: 'CABL' }, function(err, listing) {
+    if (err == null && listing != null) {
+      console.log(listing)
+      listing.remove(function(err) { });
+    }
+  });
 };
-var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+var updatePhelpsLab = function() {
+  var newAddress = "new address for Phelps Laboratory"
+  Listing.findOneAndUpdate({ name: 'Phelps Laboratory' }, { address: newAddress }, function(err, listing) {
+    if (err != null || listing == null) { return; }
+  });
+  Listing.findOne({ name: 'Phelps Laboratory' }, function(err, listing) {
+    if (err == null && listing != null) {
+      console.log(listing);
+    }
+  });
 };
-var retrieveAllListings = function() {
-  /* 
+ /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+var retrieveAllListings = function() {
+  Listing.find({}, function(err, listings) {
+    if (err == null && listings != null) {
+      console.log(listings);
+    }
+  }); 
 };
 
 findLibraryWest();
